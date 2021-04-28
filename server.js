a85f97851f9ae03e08289cf2eefc8c2d624b0617
Md.Zahid Hasan Ebu <30809719+Ebu-46@users.noreply.github.com>
2021-04-28 14:39:36 +0600

    test heroku

--- server.js
+++ server.js
@@ -42,3 +42,6 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
 })
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
