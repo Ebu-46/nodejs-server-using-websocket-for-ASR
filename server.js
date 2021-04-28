727c6a2ed8d43a719ac03de6c25df383b1912e2f
Md.Zahid Hasan Ebu <30809719+Ebu-46@users.noreply.github.com>
2021-04-28 14:28:02 +0600

    testing heroku

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-const SocketServer = require('websocket').server
-const http = require('http')
-let PORT = process.env.PORT || 3000
+// const SocketServer = require('websocket').server
+// const http = require('http')
+// let PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {})
+// const server = http.createServer((req, res) => {})
 
-server.listen(3000, ()=>{
-    console.log("Listening on port 3000...")
-})
+// server.listen(3000, ()=>{
+//     console.log("Listening on port 3000...")
+// })
 
-wsServer = new SocketServer({httpServer:server})
+// wsServer = new SocketServer({httpServer:server})
 
-const connections = []
+// const connections = []
 
-wsServer.on('request', (req) => {
-    const connection = req.accept()
-    console.log('new connection')
-    connections.push(connection)
+// wsServer.on('request', (req) => {
+//     const connection = req.accept()
+//     console.log('new connection')
+//     connections.push(connection)
 
-    connection.on('message', (mes) => {
-        connections.forEach(element => {
-            if (element != connection)
-                element.sendUTF(mes.utf8Data)
-        })
-    })
+//     connection.on('message', (mes) => {
+//         connections.forEach(element => {
+//             if (element != connection)
+//                 element.sendUTF(mes.utf8Data)
+//         })
+//     })
 
-    connection.on('close', (resCode, des) => {
-        console.log('connection closed')
-        connections.splice(connections.indexOf(connection), 1)
-    })
+//     connection.on('close', (resCode, des) => {
+//         console.log('connection closed')
+//         connections.splice(connections.indexOf(connection), 1)
+//     })
 
+// })
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
 })
